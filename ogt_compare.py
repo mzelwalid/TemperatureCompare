07e4f24077397b34a962aed1089654b0d17625bf
Mohamed El-Walid <mze99d@mail.missouri.edu>
2019-08-06 16:59:48 -0400

    Implement: Background data augmentation

--- ogt_compare.py
+++ ogt_compare.py
@@ -3,26 +3,44 @@ import itertools
 import numpy
 import tensorflow as tf
 import tensorflow.keras as k
+from random import sample
 
 #Class
 class ProteinMatrixConcatInput(k.utils.Sequence):
-	def __init__(self, x, batch_size):
+	def __init__(self, x, batch_size, pad_to):
 		self.x = x
 		self.perms = tuple(itertools.permutations(x.keys(), 2))
 		self.batch_size = batch_size
 		self.sample_size = len(self.perms)
+		self.pad_to = pad_to
 	
 	def __len__(self):
 		return int(numpy.ceil(self.sample_size / float(self.batch_size)))
 	
 	def __getitem__(self, idx):
 		#Step 1: extract appropriate permutations from self.perms
-		perms_batch
-	
+		perms_batch = self.perms[idx * self.batch_size:(idx + 1) * self.batch_size]
+		batch_x = []
+		batch_y = []
+
+		for i, pair in enumerate(perms_batch):
+			leftOGT = pair[0].split('_')[2]
+			rightOGT = pair[1].split('_')[2]
+			rightGreater = int(leftOGT < rightOGT)
+			combination = numpy.vstack((self.x[pair[0]], self.x[pair[1]]))[:,2:]
+			batch_y.append(rightGreater)
+			# pad batch_x here
+			pad = numpy.zeros((self.pad_to - combination.shape[0], 26))
+			combination = numpy.vstack((combination, pad))
+			batch_x.append(numpy.expand_dims(combination, axis = 2))
+
+		batch_x = numpy.stack(batch_x, axis = 0)
+		batch_y = numpy.asarray(batch_y)
+		return (batch_x, batch_y);
 
 
 dictionary = {}
-filename = "Ku40SecondaryStructures_with_class.txt"
+filename = "data/Ku40SecondaryStructures_with_class.txt"
 
 with open(filename) as data_file:
 	protKey = ''
@@ -35,34 +53,25 @@ with open(filename) as data_file:
 			dictionary[protKey].append(line.split("\t"))
 
 perms = itertools.permutations(dictionary.keys(), 2)
-perms_slice = list(itertools.islice(perms, 500))
 
 
 #convert lists in dictionary to numpy arrays
 numpy_dict = {}
 for key in dictionary:
 	numpy_dict[key] = numpy.asarray(dictionary[key])
-	
-x = []
-y = []
 
 # find longest concatenation in perms
 max_length = 0
-for ele in perms_slice:
+for ele in perms:
 	max_length = max(numpy_dict[ele[0]].shape[0] + numpy_dict[ele[1]].shape[0], max_length)
 
-for i, pair in enumerate(perms_slice):
-	leftOGT = pair[0].split('_')[2]
-	rightOGT = pair[1].split('_')[2]
-	rightGreater = int(leftOGT < rightOGT)
-	combination = numpy.vstack((numpy_dict[pair[0]], numpy_dict[pair[1]]))[:,2:]
-	y.append(rightGreater)
-	# pad x here
-	pad = numpy.zeros((max_length - combination.shape[0], 26))
-	combination = numpy.vstack((combination, pad))
-	x.append(numpy.expand_dims(combination, axis = 2))
+all_keys = list(numpy_dict.keys())
+num_of_val_keys = int(numpy.ceil(len(all_keys) * 0.1))
 
-x = numpy.stack(x, axis = 0)
+val_keys = sample(all_keys, num_of_val_keys)
+
+train_numpy_dict = {key: numpy_dict[key] for key in all_keys if key not in val_keys}
+val_numpy_dict = {key: numpy_dict[key] for key in all_keys if key in val_keys}
 
 
 m = k.Sequential()
@@ -89,12 +98,11 @@ m.compile(
 
 print(m.summary())
 
-history = m.fit(
-	x = x,
-	y = y,
-	batch_size = 10,
-	epochs = 40,
-	validation_split = 0.2)
+history = m.fit_generator(
+	generator = ProteinMatrixConcatInput(train_numpy_dict, 64, max_length),
+	epochs = 5,
+	validation_data = ProteinMatrixConcatInput(val_numpy_dict, 64, max_length),
+	workers = 2)
 
 print(history)
 
